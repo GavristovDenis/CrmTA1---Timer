36d9ad018317377bc20b6454d3ee1b4e4aa78c43
GavristovDenis <102148379+GavristovDenis@users.noreply.github.com>
2023-04-03 19:30:33 +0300

    comments deleted

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const inputEl = document.querySelector('input');
 const buttonEl = document.querySelector('button');
 const timerEl = document.querySelector('span');
-// Напишите реализацию createTimerAnimator
-// который будет анимировать timerEl
+
 const createTimerAnimator = () => {
     return (seconds) => {
         setInterval(() => {
